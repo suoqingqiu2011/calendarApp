2e6eb9815e12acb04ab1b3df0bb8e6858c3c465a
Glitch (calenda) <none>
2018-05-26 17:43:53 +0000

    👐🍥 Checkpoint ./server.js:812689/2120

--- server.js
+++ server.js
@@ -23,23 +23,33 @@ var db = new sqlite3.Database(dbFile);
 // if ./.data/sqlite.db does not exist, create it, otherwise print records to console
 db.serialize(function(){
   if (!exists) {
-    db.run('CREATE TABLE Dreams (dream TEXT)');
-    console.log('New table Dreams created!');
+    db.run('CREATE TABLE Users (name TEXT)');
+    console.log('New table Users created!');
+    /*db.run('CREATE TABLE Events (title TEXT)');
+    console.log('New table Events created!');*/
     
     // insert default dreams
     db.serialize(function() {
-      db.run('INSERT INTO Dreams (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")');
+      db.run('INSERT INTO Users (name) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")');
+      //db.run('INSERT INTO Events (title) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")');
     });
   }
   else {
-    console.log('Database "Dreams" ready to go!');
-    db.each('SELECT * from Dreams', function(err, row) {
+    console.log('Database "Users" ready to go!');
+    db.each('SELECT * from Users', function(err, row) {
       if ( row ) {
         console.log('record:', row);
       }
     });
+     console.log('Database "Events" ready to go!');
+    db.each('SELECT * from Events', function(err, row) {
+      if ( row ) {
+        console.log('record1:', row);
+      }
+    });*/
   }
 });
+
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
